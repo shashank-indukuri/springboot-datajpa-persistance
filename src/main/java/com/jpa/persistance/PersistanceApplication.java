72ab3bc854299ad47656e5736c37409af8a22f5a
shashank-indukuri <shashank.indukuri05@gmail.com>
2022-04-13 13:17:49 -0500

    Insert rows

--- src/main/java/com/jpa/persistance/PersistanceApplication.java
+++ src/main/java/com/jpa/persistance/PersistanceApplication.java
@@ -1,13 +1,32 @@
 package com.jpa.persistance;
 
+import java.sql.Date;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PersistanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersistanceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner saveData(StudentRepository studentRepository) {
+		return args -> {
+			Student std = new Student();
+			std.setStudentId("10");
+			std.setFirstName("Leo");
+			std.setLastName("Messi");
+			std.setEmail("leo10@gmail.com");
+			std.setAge(24);
+			std.setAdmittedDate(new Date(2020-12-12));
+
+			studentRepository.save(std);
+		};
 	}
 
 }
